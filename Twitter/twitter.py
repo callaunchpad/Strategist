5cd8e345345563ded59f4a2f215e2d39bc116cc0
Chris Wang <czwang@berkeley.edu>
2021-03-02 16:43:26 -0800

    Twitter.py changes

--- Twitter/twitter.py
+++ Twitter/twitter.py
@@ -6,19 +6,22 @@ consumer_secret = "Dk8htMfjCnmGyGfN269MHr9Da4Cy2LQer890kA9mjlwTE0CSh8"
 
 auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 api = tweepy.API(auth)
-# test authentication
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
 
+
+
+# api.search returns a list of COUNT searchtweet objects.
+# searchtweet object documentation & attributes: https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/object-model/tweet
+# 
 def searchTweet(ticker):
-    print(api.search(q = ticker, lang = "en", count = 5, result_type = "recent"))
+    result = api.search(q = ticker, lang = "en", count = 5, result_type = "recent")
+    result = [r.text for r in result]
+    return result
 
 
 def main():
-    print(searchTweet("TSLA"))
+    tweets = searchTweet("TSLA")
+    for tweet in tweets:
+        print(tweet, '\n')
 
 if __name__ == "__main__":
     main()
